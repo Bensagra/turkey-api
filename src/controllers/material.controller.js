abe549f3518c539b2effd8328116f7801c219b2f
Bensagra <bensagra@gmail.com>
2024-05-29 15:10:51 -0300

    w

--- src/controllers/material.controller.js
+++ src/controllers/material.controller.js
@@ -1,5 +1,9 @@
-const getMaterial=(req,res)=>{
-res.json("HOla - tu vieja");
+import {getConnection} from "./../database/database";
+
+const getMaterial= async(req,res)=>{
+const connection = await getConnection();
+const result = await connection.query("SELECT * FROM `Material`");
+res.json(result);
 };
 
 
