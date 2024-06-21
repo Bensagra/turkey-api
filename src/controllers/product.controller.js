bd46ea2b6e4a34047ae472dfa5689264fdc5ec1a
Bensagra <Bensagra@gmail.com>
2024-06-21 20:07:18 -0300

    chore: Add multer and multer-ftp dependencies for file upload functionality

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,6 +1,9 @@
 import { status } from "express/lib/response";
 import {getConnection} from "../database/database";
-
+const ftpStorage = require('multer-ftp');
+const ftp = require('ftp');
+const multer = require('multer');
+const ftpClient = new ftp();
 const getproduct= async(req,res)=>{
 try {
     const connection = await getConnection();
@@ -143,6 +146,30 @@ const getFromCategory = async (req, res) => {
                 
                 };
 
+                const postImage = async(req,res)=>{
+                  console.log(req.file);
+                  const upload = multer({storage, req},);
+                  exports.upload = upload.single('image');
+                }
+    ftpClient.connect({
+        port: 21,
+        host: 'ftp.turkeyhombre.com.ar',
+        user: 'images@turkeyhombre.com.ar',
+        password: 'VirreyDelPino2706'
+    });
+
+const storage = new ftpStorage({
+        basepath: 'ftp://images%2540turkeyhombre.com.ar@ftp.turkeyhombre.com.ar/images',
+        connection: ftpClient,
+        destination: (req, file, options, cb) => {
+            cb(null, './');
+        }
+    });
+
+
+
+
+
 
 export const methods = {
     getproduct,
@@ -153,5 +180,6 @@ export const methods = {
     getCategory,
     getproductById,
     getAllCategory,
-    getAllSubCategory
+    getAllSubCategory,
+    postImage
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,6 +3,8 @@ import { methods as productControllers } from "../controllers/product.controller
 
 
 const router = Router();
+const multer = require('multer');
+const path = multer({dest : 'uploads/'});
 
 
 router.get("/",productControllers.getproduct);
@@ -14,4 +16,5 @@ router.post("/",productControllers.postProduct);
 router.delete("/delete",productControllers.deleteProduct);
 router.get("/all_category", productControllers.getAllCategory);
 router.get("/all_subcategory", productControllers.getAllSubCategory);
+router.post("/image", path.single('image'), productControllers.postImage);
 export default router;
