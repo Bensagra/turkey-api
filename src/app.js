8a572e9f9de147d95e149b2ab1673616a108cdf4
Bensagra <Bensagra@gmail.com>
2024-06-23 16:31:10 -0300

    feat: Add CORS middleware to allow cross-origin requests

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ app.set("port", process.env.PORT || 3000);
 //middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use((req, res, next) => { 
+    res.setHeader('Access-Control-Allow-Origin', host) 
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept') 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS') 
+    next() 
+  }) 
 //Routes
 app.use("/api/turkey/",productRoutes);
 export default app; 
