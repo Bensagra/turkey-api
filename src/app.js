7e44e60efe1eda1de82ca71cda60daac15032271
Bensagra <Bensagra@gmail.com>
2024-06-23 16:47:30 -0300

    refactor: Update CORS middleware to allow all origins

--- src/app.js
+++ src/app.js
@@ -11,9 +11,8 @@ app.set("port", process.env.PORT || 3000);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use((req, res, next) => { 
-    res.setHeader('Access-Control-Allow-Origin', host) 
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept') 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS') 
+    res.setHeader('Access-Control-Allow-Origin', '*') 
+   
     next() 
   }) 
 //Routes
