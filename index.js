d3b9314f2468ad83666fa3df8a8521a44c0244d6
Bensagra <Bensagra@gmail.com>
2024-12-09 22:24:27 -0300

    Refactor server setup by consolidating middleware and routing in index.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-import app from "./src/app.js";
-const main=()=>{
-    app.listen(app.get("port"));
-    console.log(`Server on port ${app.get("port")}`)
-    };
+import express from 'express';
+import morgan from 'morgan';
+import productRoutes from "./src/routes/product.routes.js";
+import cors from "cors";
 
-main();
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+
+};
+
+
+app.use(cors(corsOptions));
+app.set("port", 3000);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/turkey/",productRoutes);
+/*let body = {
+  cartId: 2,
+  userId: 1
+}*/
+const startServer = () => {
+  app.listen(app.get('port'), () => {
+    console.log(`Servidor corriendo en puerto ${app.get('port')}`);
+  });
+};
+startServer()
 
 
 
+
+export default app; 
 
